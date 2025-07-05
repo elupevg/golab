c13a270e560f3806a4b61e63fa35f2aba600c9df
Evgeny Lupashko <lupashko.evgeny@gmail.com>
2025-07-05 19:07:55 +0100

    cleaned up examples, moved types into a separate file

--- internal/topology/topology.go
+++ internal/topology/topology.go
@@ -38,52 +38,6 @@ var (
 	ErrUnknownProtocol  = errors.New("unknown protocol")
 )
 
-// Topology represents a virtual network comprised of nodes and links.
-type Topology struct {
-	Name          string           `yaml:"name"`
-	Nodes         map[string]*Node `yaml:"nodes"`
-	Links         []*Link          `yaml:"links"`
-	IPStartFrom   *IPStartFrom     `yaml:"ip_start_from"`
-	ManageConfigs bool             `yaml:"manage_configs"`
-}
-
-// IPStartFrom represents a collection of initial subnets for auto-allocation.
-type IPStartFrom struct {
-	Links     []string `yaml:"links"`
-	Loopbacks []string `yaml:"loopbacks"`
-}
-
-// Node represents a node in a virtual network topology.
-type Node struct {
-	Name       string
-	Image      string   `yaml:"image"`
-	Binds      []string `yaml:"binds"`
-	Vendor     vendors.Vendor
-	Interfaces []*Interface
-	Loopbacks  []string `yaml:"loopbacks"`
-	Protocols  map[string]string
-	Enable     []string `yaml:"enable"`
-	Sysctls    map[string]string
-}
-
-// Interface respresents a network node attachment to a link.
-type Interface struct {
-	Name       string
-	Link       string
-	IPv4       string
-	IPv6       string
-	DriverOpts map[string]string
-}
-
-// Link represents a link in a virtual network topology.
-type Link struct {
-	Name       string   `yaml:"name"`
-	Endpoints  []string `yaml:"endpoints"`
-	RawSubnets []string `yaml:"ip_subnets"`
-	Subnets    []*net.IPNet
-	Gateways   []net.IP
-}
-
 // populateBinds validates/fixes user provided binds and adds vendor-specific ones.
 func (n *Node) populateBinds() error {
 	binds := make([]string, 0)
